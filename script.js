a770d528cc7b20abdb9e4e6a711b8705c4f19d6d
lukasuxo <lukasukhiashvili06@gmail.com>
2024-10-18 00:02:48 +0400

    JS1 🌐

---
+++ script.js
@@ -0,0 +1,29 @@
+//firstname//
+const firstname = "Luka"
+console.log(firstname)
+
+//lastname//
+const lastname = "Sukhiashvili"
+console.log(lastname)
+
+//number//
+let age = "18"
+console.log(age)
+
+//email//
+const email = "lukasukhiashvili06@gmail.com"
+console.log(email)
+
+//phonenumber//
+const phonenumber = "599-44-63-60"
+console.log("%c" + phonenumber, "color: red;")
+
+//true//
+const adult = true;
+console.log("%c" + adult, "color:yellow;");
+
+//false//
+const doctor = false
+console.log("%c" + doctor, "color: pink")
+
+
